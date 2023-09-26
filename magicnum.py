37573f3d912d2d9a0ace8b55a98ec401931c54a6
RootExE <57043502+msf-Root-ExE@users.noreply.github.com>
2023-09-26 21:39:59 +0100

    Update magicnum.py

--- magicnum.py
+++ magicnum.py
@@ -30,7 +30,6 @@ MAGIC_NUMBERS = {
     "ppt": b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1',
     "pptx": b'PK\x03\x04',
     "mdb": b'\x00\x01\x00\x00Standard Jet DB',
-    # Text-based file formats without unique magic numbers
     "txt": None,
     "md": None,
     "csv": None,
@@ -42,46 +41,71 @@ MAGIC_NUMBERS = {
     "conf": None
 }
 
-def list_files():
-    files = [f for f in os.listdir('.') if os.path.isfile(f)]
-    for i, file in enumerate(files):
-        print(f"{i + 1}. {file}")
-    return files
+def list_files(directory):
+    try:
+        files = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+        for i, file in enumerate(files):
+            print(f"{i + 1}. {file}")
+        return files
+    except Exception as e:
+        print(f"Error accessing directory: {e}")
+        return []
 
 def main():
-    print("Available files:")
-    files = list_files()
+    print("Select a directory:")
+    print("1. Current Directory")
+    print("2. Desktop")
+    print("3. Root Directory")
+    dir_choice = int(input("Enter the number corresponding to the directory: "))
+    
+    if dir_choice == 1:
+        directory = "."
+    elif dir_choice == 2:
+        directory = os.path.join(os.path.expanduser('~'), 'Desktop')
+    elif dir_choice == 3:
+        directory = "/"
+    else:
+        print("Invalid choice.")
+        return
+    
+    print(f"Available files in {directory}:")
+    files = list_files(directory)
 
-    # Ask the user to select a file
+    if not files:
+        return
+
     file_index = int(input("Select a file by entering its number: ")) - 1
     if file_index < 0 or file_index >= len(files):
         print("Invalid selection.")
         return
-
+    
     filename = files[file_index]
 
-    # Show available extensions and their corresponding magic numbers
     print("Available Extensions:")
     for ext in MAGIC_NUMBERS.keys():
         print(ext)
 
-    # Ask the user for the target extension
     tgt_ext = input("Enter the target file extension from the available list: ")
     if tgt_ext not in MAGIC_NUMBERS:
         print("Invalid extension.")
         return
-
-    # Generate the new file name
+    
     new_filename = os.path.splitext(filename)[0] + '.' + tgt_ext
-
-    # If the target extension has an associated magic number, modify the file's magic number
+    
     if MAGIC_NUMBERS[tgt_ext]:
-        with open(filename, 'r+b') as file:
-            file.write(MAGIC_NUMBERS[tgt_ext])
+        try:
+            with open(filename, 'r+b') as file:
+                file.write(MAGIC_NUMBERS[tgt_ext])
+        except Exception as e:
+            print(f"Error modifying file: {e}")
+            return
 
-    # Rename the file
-    os.rename(filename, new_filename)
-    print(f"Changed {'' if MAGIC_NUMBERS[tgt_ext] else 'extension and '}renamed: {filename} -> {new_filename}")
+    try:
+        os.rename(filename, new_filename)
+        print(f"Changed {'' if MAGIC_NUMBERS[tgt_ext] else 'extension and '}renamed: {filename} -> {new_filename}")
+    except Exception as e:
+        print(f"Error renaming file: {e}")
 
 if __name__ == "__main__":
     main()
+
